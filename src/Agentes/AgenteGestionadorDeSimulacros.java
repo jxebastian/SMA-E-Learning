3b54b582e5493563598c16a1d16c412422d042e0
fcortesn <pipecrtd@gmail.com>
2019-07-30 19:16:41 -0500

    holi

--- src/Agentes/AgenteGestionadorDeSimulacros.java
+++ src/Agentes/AgenteGestionadorDeSimulacros.java
@@ -5,6 +5,6 @@ import jade.core.Agent;
 public class AgenteGestionadorDeSimulacros extends Agent{
     @Override
     protected void setup() {
-        
+        //Esto es sólo para probar
     }
 }
